8d3baef8d9f780674314a57e532708d4dde4f91d
YaozhenghangMa <Yaozhenghang.Ma@gmail.com>
2023-06-28 16:23:02 +0800

    collective communication

--- include/messaging/comm_world.hpp
+++ include/messaging/comm_world.hpp
@@ -29,10 +29,48 @@ namespace messaging {
 
         template<typename T>
         int Recv(T &data, int source, int tag);
+
+        template<typename T>
+        int Broadcast(T &data, int root);
+
+        template<typename T>
+        int Gather(T &data, std::vector<T> &all_data, int root);
+
+        template<typename T>
+        int Scatter(std::vector<T> &all_data, T &data, int root);
     private:
         messaging::Environment env_;
         bool initialized_;
     };
+
+    template<typename T>
+    std::vector<std::vector<T>> split_vector(std::vector<T> &data, int length, int size) {
+        std::vector<std::vector<T>> split_data;
+        split_data.resize(size);
+        int index = 0;
+        for(int i=0; i<size; i++) {
+            split_data[i].resize(length);
+            for(int j=0; j<length; j++) {
+                split_data[i][j] = data[index];
+                index++;
+            }
+        }
+        return split_data;
+    }
+
+    template<typename T>
+    std::vector<T> merge_vector(std::vector<std::vector<T>> &data, int length, int size) {
+        std::vector<T> merge_data;
+        merge_data.resize(length*size);
+        int index = 0;
+        for(int i=0; i<size; i++) {
+            for(int j=0; j<length; j++) {
+                merge_data[index] = data[i][j];
+                index ++;
+            }
+        }
+        return merge_data;
+    }
 
     template<typename T>
     int CommWorld::Send(T &data, int destination, int tag) {
@@ -49,8 +87,58 @@ namespace messaging {
         MPI_Status status;
         std::vector<std::byte> serialized_data;
         MPI_Recv(&length, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-        serialized_data.resize(length, std::byte{0});
+        serialized_data.resize(length);
         MPI_Recv(&serialized_data[0], length, MPI_BYTE, source, tag, MPI_COMM_WORLD, &status);
+        serialization::deserialize(data, serialized_data);
+        return 0;
+    }
+
+    template<typename T>
+    int CommWorld::Broadcast(T &data, int root) {
+        int length=0;
+        auto serialized_data = serialization::serialize<T>(data);
+        length = serialized_data.size();
+        MPI_Bcast(&length, 1, MPI_INT, root, MPI_COMM_WORLD);
+        serialized_data.resize(length);
+        MPI_Bcast(&serialized_data[0], length, MPI_BYTE, root, MPI_COMM_WORLD);
+        serialization::deserialize(data, serialized_data);
+        return 0;
+    }
+
+    template<typename T>
+    int CommWorld::Gather(T &data, std::vector<T> &all_data, int root) {
+        auto serialized_data = serialization::serialize<T>(data);
+        int length = serialized_data.size();
+        std::vector<std::byte> all_serialized_data;
+        all_serialized_data.resize(this->Size() * length);
+        MPI_Gather(&serialized_data[0], length, MPI_BYTE, &all_serialized_data[0], length, MPI_BYTE, root, MPI_COMM_WORLD);
+        if(this->Rank() == root) {
+            auto split_serialized_data = split_vector<std::byte>(all_serialized_data, length, this->Size());
+            all_data.resize(this->Size());
+            for(int i=0; i<this->Size(); i++) {
+                serialization::deserialize(all_data[i], split_serialized_data[i]);
+            }
+        }
+        return 0;
+    }
+
+    template<typename T>
+    int CommWorld::Scatter(std::vector<T> &all_data, T &data, int root) {
+        int length = 0;
+        std::vector<std::byte> merge_all_serialized_data;
+        std::vector<std::byte> serialized_data;
+        if(this->Rank() == root) {
+            std::vector<std::vector<std::byte>> all_serialized_data;
+            all_serialized_data.resize(all_data.size());
+            for(int i=0; i<all_data.size(); i++) {
+                all_serialized_data[i] = serialization::serialize(all_data[i]);
+            }
+            length = int(all_serialized_data[0].size());
+            merge_all_serialized_data = merge_vector<std::byte>(all_serialized_data, length, this->Size());
+        }
+        this->Broadcast(length, root);
+        serialized_data.resize(length);
+        MPI_Scatter(&merge_all_serialized_data[0], length, MPI_BYTE, &serialized_data[0], length, MPI_BYTE, root, MPI_COMM_WORLD);
         serialization::deserialize(data, serialized_data);
         return 0;
     }
---
+++ test/test_collective.cpp
@@ -0,0 +1,65 @@
+/*
+ * @Author: Yaozhenghang.Ma Yaozhenghang.Ma@gmail.com
+ * @Date: 2023-06-28
+ * @FilePath: test/test_collective.cpp
+ * @Description:
+ *  
+ * Copyright (c) 2023 by Yaozhenghang.Ma email:Yaozhenghang.Ma@gmail.com, All Rights Reserved.
+ */
+
+#include <stdio.h>
+#include <messaging.hpp>
+
+class Coordinate {
+public:
+    double x;
+    double y;
+    double z;
+};
+
+int main() {
+    messaging::CommWorld comm;
+    Coordinate coord;
+    std::vector<Coordinate> coord_vec;
+    int rank = comm.Rank();
+    int size = comm.Size();
+
+    // Test broadcast
+    if(rank == 0) {
+        coord.x = 1;
+        coord.y = 2;
+        coord.z = 3;
+    }
+    comm.Broadcast(coord, 0);
+    assert(coord.x == 1);
+    assert(coord.y == 2);
+    assert(coord.z == 3);
+
+    // Test Gather
+    coord.x = rank;
+    coord.y = rank+10;
+    coord.z = rank+100;
+    comm.Gather(coord, coord_vec, 0);
+    if(rank == 0) {
+        for(int i=0; i<size; i++) {
+            assert(coord_vec[i].x == i);
+            assert(coord_vec[i].y == i+10);
+            assert(coord_vec[i].z == i+100);
+        }
+    }
+
+
+    // Test Scatter
+    if(rank == 0) {
+        for(int i=0; i<size; i++) {
+            coord_vec[i].x = i;
+            coord_vec[i].y = i+100;
+            coord_vec[i].z = i+1000;
+        }
+    }
+    comm.Scatter(coord_vec, coord, 0);
+    assert(coord.x == rank);
+    assert(coord.y == rank+100);
+    assert(coord.z == rank+1000);
+    return 0;
+}
